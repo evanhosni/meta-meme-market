e004b8535d7084d1d8bbb9b53a373270bd0b1915
jwhector <jared.hector@gmail.com>
2021-11-01 15:56:17 -0700

    Change share state after 24h

---
+++ market/shareListing.js
@@ -0,0 +1,29 @@
+var CronJob = require('cron').CronJob;
+const { Meme } = require('../models/Meme');
+
+async function checkIncubating() {
+    try {
+        const incubating = await Meme.findAll({ where: { is_initial: true } });
+        incubating.forEach(meme => {
+            const publicDate = new Date(meme.created_at); // Replace with date created row name.
+            publicDate.setDate(publicDate.getDate() + 1);
+
+            if (Date(meme.created_at) < Date.now()) {
+                meme.update({ is_initial: false });
+                // Add 'go public.'
+            } else {
+                const job = new CronJob(publicDate, () => {
+                    meme.update({ is_initial: false });
+                    // Add 'go public'.
+                    job.stop();
+                });
+            }
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function releaseShares() {
+    
+}
