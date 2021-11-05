306eb449658e28584d6c721eaf6076d2abd616f7
jwhector <jared.hector@gmail.com>
2021-11-04 17:33:13 -0700

    Display balance and share value

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { Meme, User, Share } = require('../models');
+const { Op } = require('sequelize');
 
 //Homepage shows all memes sorted descending by created most recently
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const balance = (await User.findByPk(req.session.user.id)).balance;
     Meme.findAll({
         order: [
             ["created_at", 'DESC']
@@ -12,13 +14,35 @@ router.get("/", (req, res) => {
         include: [{
             model: User,
             attributes: ['username']
+        }, {
+            model: Share,
+            where: {
+                [Op.and]: {
+                    [Op.not]: { listed_at: null },
+                    // [Op.not]: { user_id: req.session.user.id }
+                }
+            },
+            order: [
+                ['bought_price', 'ASC'],
+                ['listed_at', 'ASC']
+            ],
+            limit: 1,
+            required: false
         }]
     }).then(memeData => {
-        const hbsMemes = memeData.map(meme => meme.get({ plain: true }))
+        const hbsMemes = memeData.map(meme => {
+            const plainMeme = meme.get({ plain: true });
+            if (plainMeme.shares.length) {
+                plainMeme.value = plainMeme.shares[0].bought_price;
+            } else {
+                plainMeme.value = null;
+            }
+            return plainMeme;
+        });
         // res.json(hbsMemes)
         res.render("home", {
-            ...hbsMemes, loggedIn: req.session.loggedIn, currentUser: req.session.user.username,
-            memes: hbsMemes
+            ...hbsMemes, loggedIn: req.session.loggedIn, currentUser: req.session.user,
+            memes: hbsMemes, balance
         })
     }).catch(err => {
         console.log(err)
@@ -26,8 +50,9 @@ router.get("/", (req, res) => {
     })
 })
 
-router.get("/meme/:id", (req, res) => {//TODO change id to title so it's "/meme/:title"
+router.get("/meme/:id", async (req, res) => {//TODO change id to title so it's "/meme/:title"
     // res.render("meme")
+    const balance = (await User.findByPk(req.session.user.id)).balance;
     Meme.findOne({
         where: {
             id: req.params.id
@@ -47,36 +72,62 @@ router.get("/meme/:id", (req, res) => {//TODO change id to title so it's "/meme/
         const hbsMeme = memeData.get({plain:true})
         // res.json(hbsMemes)
         res.render("meme", {
-            ...hbsMeme, loggedIn: req.session.loggedIn, currentUser: req.session.user.username,
-            meme: hbsMeme
-        })
+            ...hbsMeme, loggedIn: req.session.loggedIn, currentUser: req.session.user,
+            meme: hbsMeme, balance
+        });
     }).catch(err => {
         console.log(err)
         res.status(500).json(err)
     })
 })
 
-router.get("/user/:username", (req, res) => {
+router.get("/user/:username", async (req, res) => {
+    const balance = (await User.findByPk(req.session.user.id)).balance;
     User.findOne({
         where: {
             username: req.params.username
         },
+        attributes: ['username'],
         include: [{
             model: Meme,
-            attributes: ['id','img','share_price','number_shares','user_id']
+            attributes: ['id','img','share_price','number_shares','user_id'],
+            include: {
+                model: Share,
+                where: {
+                    [Op.and]: {
+                        [Op.not]: { listed_at: null },
+                        // [Op.not]: { user_id: req.session.user.id }
+                    }
+                },
+                order: [
+                    ['bought_price', 'ASC'],
+                    ['listed_at', 'ASC']
+                ],
+                limit: 1,
+                required: false
+            }
         }]
     }).then(userData => {
-        console.log(userData.toJSON())
-        const hbsUser = userData.get({plain:true})
+        // const hbsUser = userData.get({plain:true})
+        const plainUser = userData.get({ plain: true });
+        for (meme of plainUser.memes) {
+            if (meme.shares.length) {
+                meme.value = meme.shares[0].bought_price;
+            } else {
+                meme.value = null;
+            }
+        }
+        console.log({...plainUser, loggedIn: req.session.loggedIn, currentUser: req.session.user, sameUser: false,
+            user: plainUser, balance})
         if(req.session.loggedIn && req.session.user.username == req.params.username) {
             res.render("user", {
-                ...hbsUser, loggedIn: req.session.loggedIn, currentUser: req.session.user.username, sameUser: true,
-                user: hbsUser
+                plainUser, loggedIn: req.session.loggedIn, currentUser: req.session.user, sameUser: true,
+                user: plainUser, balance
             })
         } else {
             res.render("user", {
-                ...hbsUser, loggedIn: req.session.loggedIn, currentUser: req.session.user.username, sameUser: false,
-                user: hbsUser
+                ...plainUser, loggedIn: req.session.loggedIn, currentUser: req.session.user, sameUser: false,
+                user: plainUser, balance
             })
         }
     }).catch(err => {
