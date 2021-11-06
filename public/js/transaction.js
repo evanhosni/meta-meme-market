40c842e8aa9c7df226f4a9dc946df42306fc4f32
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-11-06 16:23:36 -0700

    failing so hard to get this modal to fkn operate

--- public/js/transaction.js
+++ public/js/transaction.js
@@ -1,2 +1,24 @@
-const buyButton = document.querySelector('#buy-button');
+var modal = document.getElementById("buyModal");
 
+// Get the button that opens the modal
+var buyBtn = document.getElementById("buyMe");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeBuy")[0];
+
+// When the user clicks on the button, open the modal
+buyBtn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
