05972968f38a8fe42552239a14cb99436d982b46
Ryan Allen <ryan.c.allen.work@gmail.com>
2021-11-06 17:46:53 -0700

    working on table etc

--- public/js/transaction.js
+++ public/js/transaction.js
@@ -1,24 +1,5 @@
-var modal = document.getElementById("buyModal");
+// const { TableHints } = require("sequelize/types")
+function toggleTable() {
 
-// Get the button that opens the modal
-var buyBtn = document.getElementById("buyMe");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("closeBuy")[0];
-
-// When the user clicks on the button, open the modal
-buyBtn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+document.getElementById("prices").classList.toggle("hidden");
 }
