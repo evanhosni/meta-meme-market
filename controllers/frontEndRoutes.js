e58d4a6adf4fd1332bb139eb8e51fbb5ad92ad0a
David Abbey <davidabbey@earthlink.net>
2021-11-07 16:02:50 -0800

    made logout cookie destroy into a redirect function instead of both being run at same time sometimes causing redirect to finish ahead of destroy leaving username on homepage until manual refresh

--- controllers/frontEndRoutes.js
+++ controllers/frontEndRoutes.js
@@ -205,8 +205,9 @@ router.get('/sell', async (req, res) => {
 })
 
 router.get('/logout', (req, res) => {
-    req.session.destroy()
-    res.redirect('/')
+    req.session.destroy(()=>{
+        res.redirect('/')
+    })
 })
 
 router.get('*', (req, res) => {
